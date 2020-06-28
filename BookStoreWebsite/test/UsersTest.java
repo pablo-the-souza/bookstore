e0a73f7889fb4deb08002268d26aa19c5c9d6026
paguirre82 <pablo.the.souza@gmail.com>
2020-06-28 10:20:24 +1000

    First Commit

---
+++ BookStoreWebsite/test/UsersTest.java
@@ -0,0 +1,29 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.bookstore.entity.Users;
+
+public class UsersTest {
+
+	public static void main(String[] args) {
+		Users user1 = new Users();
+		user1.setEmail("pablo.the.souza@gmail.com");
+		user1.setEmail("Pablo Souza");
+		user1.setEmail("123456");
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		entityManager.persist(user1);
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
+		
+		System.out.println("A users object was persisted");
+	}
+
+}
